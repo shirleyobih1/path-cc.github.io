227da38946d0f023b1d5e809f35b02bead19ddac
Cannon Lock <clock@wisc.edu>
2021-07-26 13:31:00 -0500

    Delete Docs Page

--- docs/run.sh
+++
@@ -1,7 +0,0 @@
-docker container kill $(docker ps | awk '/path-cc-website/ {print $1}')
-
-PORT=8003
-
-docker run --name path-cc-website -p $PORT:$PORT --rm --volume $PWD:/srv/jekyll -it jekyll/jekyll:latest /bin/sh
-
-jekyll serve --watch -H 0.0.0.0 -P 8003
